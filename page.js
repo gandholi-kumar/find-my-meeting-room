5122dea2274e78bdcd8387eb23ade2886e9a240c
gandholi-kumar <saiarunkumar.gandholi@philips.com>
2025-02-18 00:34:53 +0530

    Updated the room ids

--- page.js
+++ page.js
@@ -1,16 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const input = document.getElementById("cell-id");
+  const input = document.getElementById("input-meeting-room");
   const cells = document.querySelectorAll(".cell");
 
+  input.focus();
   input.addEventListener("input", () => {
     // Clear all highlights
     cells.forEach((cell) => cell.classList.remove("highlight"));
 
     const id = input.value.trim().toLowerCase();
+
+    if (id === "") {
+      input.style.border = "none";
+      return;
+    }
+
     const cell = document.getElementById(`cell-${id}`);
 
     if (cell) {
       cell.classList.add("highlight");
+      input.style.borderColor = "green";
+      input.style.borderStyle = "solid";
+    } else {
+      console.log("else ");
+      input.style.borderColor = "red";
     }
   });
 });
