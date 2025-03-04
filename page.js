5fb7b6b8b410bcc820cfa5b9ef8697bcff300596
Shivaprasad84 <shivaprasadsmys@gmail.com>
2025-03-04 12:57:06 +0530

    Enhance cell highlighting functionality with location mapping

--- page.js
+++ page.js
@@ -7,22 +7,83 @@ document.addEventListener("DOMContentLoaded", () => {
     // Clear all highlights
     cells.forEach((cell) => cell.classList.remove("highlight"));
 
-    const id = input.value.trim().toLowerCase();
+    let id = input.value.trim().toLowerCase();
+
+    const locationMap = new Map([
+      ["longitude", "CR06"],
+      ["latitude", "CR07"],
+      ["landfall", "CR08"],
+      ["oyster", "FR13"],
+      ["pondweed", "FR17"],
+      ["gabbro", "FR18"],
+      ["hydrilla", "FR19"],
+      ["brook", "FR20"],
+      ["creek", "FR21"],
+      ["rill", "FR22"],
+      ["jetstream", "MR10"],
+      ["wave", "MR15"],
+      ["aalto", "MR16"],
+      ["samudra", "MR17"],
+      ["mahasagar", "MR18"],
+      ["paniyam", "MR19"],
+      ["collab area work cafe", "Collab Area Work Cafe"],
+      ["da04", "DA04"],
+      ["polaris", "CR01"],
+      ["waypoint", "CR02"],
+      ["aquamarine", "FR01"],
+      ["larimar", "FR02"],
+      ["pearl", "FR03"],
+      ["abalone", "FR04"],
+      ["coral", "FR05"],
+      ["agate", "FR06"],
+      ["current", "MR01"],
+      ["flow", "MR02"],
+      ["torrent", "MR03"],
+      ["cascade", "MR04"],
+      ["rapids", "MR05"],
+      ["da01", "DA01"],
+      ["gps", "CR05"],
+      ["hyacinth", "FR14"],
+      ["basalt", "FR15"],
+      ["kelps", "FR16"],
+      ["riverine", "MR11"],
+      ["streamline", "MR12"],
+      ["splash", "MR13"],
+      ["bayview", "MR14"],
+      ["da03", "DA03"]
+    ]);
 
     if (id === "") {
       input.style.border = "none";
       return;
     }
 
-    const cell = document.getElementById(`cell-${id}`);
-
-    if (cell) {
+    const cellFound = (cell) => {
       cell.classList.add("highlight");
       input.style.borderColor = "green";
       input.style.borderStyle = "solid";
-    } else {
-      console.log("else ");
+    }
+
+    const cellNotFound = () => {
       input.style.borderColor = "red";
+    }
+
+    const findCellFromName = (name) => {
+      id = locationMap.get(name).toLowerCase();
+      return document.getElementById(`cell-${id}`);
+    }
+
+    let cell = document.getElementById(`cell-${id}`);
+
+    if (cell) {
+      cellFound(cell);
+    } else {
+      cell = findCellFromName(id);
+      if (cell) {
+        cellFound(cell);
+      } else {
+        cellNotFound();
+      }
     }
   });
 });
